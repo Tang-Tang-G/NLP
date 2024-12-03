17337e803e728f7971b18e8285193ecbee453cc2
TH <3176089369@qq.com>
2024-12-03 21:53:13 +0800

    index/index.vue UI finish

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,26 +1,24 @@
 <template>
 	<div class="app-container">
-		<div class="input-container">
-			<view @click="copyPaste">点击我，分析对方的情绪</view>
-		</div>
+		<view class="input-container" @click="copyPaste">
+			<view >点击我，分析对方的情绪</view>
+		</view>
 
-		<!-- 主要模块 -->
 		<div class="main-modules">
 			<div class="module" v-for="module in mainModules" :key="module.name">
-				<div class="module-header">
-					<img :src="module.icon" :alt="module.name" />
-					<h3>{{ module.name }}</h3>
-				</div>
-				<p>{{ module.description }}</p>
+				<img :src="module.image" :alt="module.name" class="image_class" />
 			</div>
 		</div>
 
-		<!-- 学院 -->
 		<div class="colleges">
+			<h3 style="padding-left: 3% ; color: gray; padding-bottom:5px;">爱情学院</h3>
 			<div class="college" v-for="college in colleges" :key="college.name">
-				<h3>{{ college.name }}</h3>
-				<div class="sections">
-					<span v-for="section in college.sections" :key="section">{{ section }}</span>
+				<img :src="college.icon" alt="学院图标" style="margin-right: 10px;" />
+				<div class='college-info'>
+					<h3>{{ college.name }}</h3>
+					<div class="sections">
+						<span v-for="section in college.sections" :key="section">{{ section }}</span>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -36,29 +34,32 @@
 
 	const mainModules = ref([{
 			name: '舔狗日志',
-			icon: 'static/logo.png',
-			description: '愿世间没有舔狗'
+			image: 'static/Dog.png'
 		},
 		{
 			name: '土味情话',
-			icon: 'static/logo.png',
-			description: '模拟真人聊天'
+			image: 'static/Love_Saying.png'
 		},
 	]);
 
 	const colleges = ref([{
-			name: '爱情学院',
-			sections: ['暗恋', '聊天搭讪', '社交撩妹']
+			name: '单身学院',
+			sections: ['暗恋', '聊天搭讪', '社交撩妹'],
+			icon: 'static/Arrow_Love.png'
 		},
 		{
-			name: '单身学院',
-			sections: ['吸引异性', '约会', '表白']
+			name: '追求学院',
+			sections: ['吸引异性', '约会', '表白'],
+			icon: 'static/Pursue.png'
 		},
-		// ...其他学院
+		{
+			name: '恋爱学院',
+			sections: ['了解异性', '分析情绪'],
+			icon: 'static/Fall_In_Love.png'
+		},
 	]);
 
 	const copyPaste = () => {
-		// 实现复制粘贴的逻辑
 		uni.navigateTo({
 			url: '/pages/ChatInput/ChatInput'
 		})
@@ -80,45 +81,48 @@
 		background-color: lightgrey;
 		border-radius: 16px;
 	}
+
 	.input-container view {
 		padding: 8px 16px;
 		color: white;
 		border: none;
 		cursor: pointer;
 	}
+
 	.main-modules {
 		display: flex;
-		flex-wrap: wrap;
+		flex-wrap: nowrap;
 		gap: 16px;
 		margin-bottom: 16px;
 	}
 
 	.module {
-		background-color: white;
-		padding: 16px;
-		border-radius: 8px;
-		width: calc(50% - 8px);
-		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-		text-align: center;
-	}
-
-	.module-header {
 		display: flex;
-		align-items: center;
-		justify-content: center;
-		margin-bottom: 8px;
+		width: 40%;
+		box-sizing: border-box;
+		margin-left: 5%;
 	}
 
-	.module-header img {
-		width: 24px;
-		height: 24px;
-		margin-right: 8px;
+	.image_class {
+		width: auto;
+		height: 70px;
+		padding: 5px;
+		border-radius: 18px;
 	}
+
 	.colleges {
 		margin-bottom: 16px;
+		font-size: 16px;
+		font-style: italic;
+		font-family: '黑体';
+		text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
+
 	}
 
 	.college {
+		align-items: center;
+		display: flex;
+		font-family: 'Courier New', Courier, monospace;
 		background-color: white;
 		padding: 16px;
 		border-radius: 8px;
@@ -126,7 +130,16 @@
 		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 	}
 
+	.college img {
+		width: 50px;
+		/* 根据需要调整图片大小 */
+		height: 50px;
+		margin-right: 10px;
+	}
+
 	.college h3 {
+		font-family: 'Courier New';
+		font-size: 20px;
 		margin-bottom: 8px;
 	}
 
@@ -137,6 +150,9 @@
 		border-radius: 4px;
 		margin-right: 4px;
 		margin-bottom: 4px;
+		font-size: 15px;
+		color: #555;
+		margin: 5;
 	}
 
 	.bottom-nav {
